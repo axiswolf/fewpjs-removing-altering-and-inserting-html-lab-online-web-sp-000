d1949887a9c123fb4d4326085a5c6d700b536d1b
Karen V <karenvo@usc.edu>
2019-12-27 09:02:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 let newHeader = document.createElement('h1')
 newHeader.id = "victory"
-newHeader.innerText = "Karen is the champion"
+newHeader.innerHTML = "Karen is the champion"
